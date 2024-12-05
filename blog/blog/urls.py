83070622d28f508ecc250b9766dd4d7f2212c39f
IDIATOU TOURE <idia4516@gmail.com>
2024-12-05 12:58:03 +0000

    feat:config for deploiement

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appblog.urls'))
