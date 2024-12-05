79bf2e8f32ab2691a90fd7cd4c510ec950071a6d
IDIATOU TOURE <idia4516@gmail.com>
2024-12-05 15:32:48 +0000

    Initial commit de mon projet

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+from waitress import serve
+
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.wsgi')
 
 application = get_wsgi_application()
